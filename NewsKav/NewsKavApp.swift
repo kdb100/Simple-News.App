c573e420f990cf0f349d36840a14f2ca4a223a41
kdb100 <oneblueproject@gmail.com>
2021-05-03 23:06:44 -0400

    Interface & JSON API update

---
+++ NewsKav/NewsKavApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsKavApp.swift
+//  NewsKav
+//
+//  Created by KDB on 2021-05-02.
+//
+
+import SwiftUI
+
+@main
+struct NewsKavApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
